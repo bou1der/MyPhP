2974b210b52d82cd575b474b45695db8568bd752
bou1der <krowkin@bk.ru>
2023-11-26 23:10:41 +0300

    A

--- index.php
+++ index.php
@@ -2,23 +2,45 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="main.css">
-
+    <title>Cute Form</title>
 </head>
 <body>
-    <form class="form" action="/PhP/reg.php" method="post" enctype = "multipart/form-data" >
-        
-        <label>Ваше имя:<input type="text" name="name"></label>
-        <label>Ваша фамилия:<input type="text" name="surname"></label>
-        <label>Группа<input type="text" name="group"></label>
-        <label>Дата рождения<input type="date" name="birthday"></label>
-        <p>Ваш аватар:</p><input type="file" value = "Send file" name = "Sendfile">
-        <input type="submit" value="Send" name="Send">
+    <article class="block1">
+        <div class="mainCard">
+            <div class="inCard">
+                <aside class="register">
+                    <div class="inRegister">
+                        <img src="/resource/item/UωU.svg" alt="">
+                        <form action="/Php/reg.php" method="post">
+                            <input type="text" placeholder="Login!" name="login">
+                            <input type="password" placeholder="Password!" name="password">
+                            <input type="password" placeholder="Repit-pass!" name="repitPass">
+                        </form>
+                        <form action="/Php/reg.php" method="post">
+                            <button type="submit" name="send">Register!!</button>
+                        </form>
+                    </div>
+                </aside>
+                <aside class="login">
+                    <div class="inLogin">
+                        <img src="/resource/item/_ω_.svg" alt="">
+                        <form action="/PhP/login.php">
+                            <input type="text" placeholder="Login!" name="login">
+                            <input type="password" placeholder="Password!" name="password">
+                            <input type="text" style="opacity: 0; cursor:default;">
+                        </form>
+                        <form action="login.php" method="post">
+                            <button type="submit" name="Log">Log-in!!</button>
+                        </form>
+                    </div>
+                </aside>
+            </div>
+        </div>
+    </article>
 
-    </form>
-
-    <script src='main.js'></script>
+<script src="main.js"></script>
 </body>
 </html>
