47c16e9a7c517ff6f803abb1c9f2801681a8e2d3
bou1der <krowkin@bk.ru>
2023-11-28 13:47:34 +0300

    full verst

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let i;
+document.querySelector('.Log > button').addEventListener('click', function(){
+    const swipePanel = document.querySelector('.plate');
+    
+    i++
+    swipePanel.style.right = (525) + 'px' ;
+    
+});
