b2e288db8a128c1f92593fdc303564adc7a0124b
bou1der <krowkin@bk.ru>
2023-11-29 00:44:20 +0300

    html+css

--- index.php
+++ index.php
@@ -2,23 +2,311 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="main.css">
-
+    <title>Cute Form</title>
 </head>
 <body>
-    <form class="form" action="/PhP/reg.php" method="post" enctype = "multipart/form-data" >
-        
-        <label>Ваше имя:<input type="text" name="name"></label>
-        <label>Ваша фамилия:<input type="text" name="surname"></label>
-        <label>Группа<input type="text" name="group"></label>
-        <label>Дата рождения<input type="date" name="birthday"></label>
-        <p>Ваш аватар:</p><input type="file" value = "Send file" name = "Sendfile">
-        <input type="submit" value="Send" name="Send">
+    <article class="block1">
+        <div class="carousel">
+            <div class="content-carousel">
+                
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+        </div>
+        <div class="carousel-2">
+            <div class="content-carousel">
+                
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+        </div>
+        <div class="carousel-3">
+            <div class="content-carousel">
+                
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+            <div class="content-carousel">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+                <img src="/resource/background/^•v•^.svg" alt="">
+                <img src="/resource/background/ó﹏ò.svg" alt="">
+                <img src="/resource/background/•̀ ᴖ •́.svg" alt="">
+                <img src="/resource/background/•‿•.svg" alt="">
+                <img src="/resource/background/•ﻌ•.svg" alt="">
+                <img src="/resource/background/,_﹏_,.svg" alt="">
+            </div>
+        </div>
+        <div class="mainCard">
+            <div class="inCard">
+                <aside class="register">
+                    <div class="inRegister">
+                        <img src="/resource/item/UωU.svg" alt="">
+                        <form action="/Php/reg.php" method="post">
+                            <input type="text" placeholder="Login!" name="login">
+                            <input type="password" placeholder="Password!" name="password">
+                            <input type="password" placeholder="Repit-pass!" name="repitPass">
+                        </form>
+                        <form action="/Php/reg.php" method="post">
+                            <button type="submit" name="send">Register!!</button>
+                        </form>
+                    </div>
+                </aside>
+                <aside class="login">
+                    <div class="inLogin">
+                        <img src="/resource/item/_ω_.svg" alt="">
+                        <form action="/PhP/login.php">
+                            <input type="text" placeholder="Login!" name="login">
+                            <input type="password" placeholder="Password!" name="password">
+                            <input type="text" style="opacity: 0; cursor:default;">
+                        </form>
+                        <form action="login.php" method="post">
+                            <button type="submit" name="Log">Log-in!!</button>
+                        </form>
+                    </div>
+                </aside>
+            
+                    <div class="front">
+                        <div class="Log"><button>Log-in</button> <div></div></div>
+                        <div>
+                            <p>Password rule!!:</p>
+                            <p>The minimum length for your lovely password is 8 characters! Isn't it exciting?</p>
+                            <p>Don't forget to add some special characters to make it more unique and special! How about sprinkling in some cool symbols like (!#$%^&*)? They'll give your password a charming touch!</p>
+                            <p>Remember, it's essential for your password to be complex. After all, you wouldn't want any unwanted intrusions, would you? Let's keep it super secure!</p>
+                            <p>Lastly, kindly avoid using spaces in your password. We want to ensure it remains perfectly snug and tidy.</p>
+                            <div class="separator"></div>
+                            <div class="anotherLog">
+                                <p>If you'd like, there is another delightful way for you to register!</p>
+                                <img src="/resource/item/(ó﹏ò｡).svg" alt="">
+                            </div>
+                        </div>
+                        <div class="links">
+                            <a href="https://www.google.ru/webhp?hl"><img src="/resource/item/google.svg" alt=""></a>
+                            <a href="https://web.telegram.org"><img src="/resource/item/telegram.svg" alt=""></a>
+                            <a href="https://en.wikipedia.org/wiki/Facebook"><img src="/resource/item/facebook.svg" alt=""></a>
+                            <a href="https://ru.wikipedia.org/wiki/Твиттер"><img src="/resource/item/twitter.svg" alt=""></a>
+                            <a href="https://vk.com/nir4y"><img src="/resource/item/Vk.svg" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="back">
+                        <div class="inBack">
+                            <div class="Log"><button>Reg-in</button></div>
+                            <div class="backTxt">
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque neque, esse id nihil earum totam voluptatem iste quasi voluptas tempore consequuntur quam aperiam repellendus sapiente autem voluptatum aliquam error soluta. Voluptatibus, voluptate omnis alias, iste veritatis dolores numquam provident, maiores hic dolorum explicabo iure ratione tempore quia quam voluptatum! Repellendus nemo quod iure quia vero, dolore aliquam labore aliquid, quo a, molestiae neque ratione. Tempore nobis perspiciatis deserunt praesentium ab.
+                                </p>
+                            </div>
+                            <div class="links">
+                                <a href="https://www.google.ru/webhp?hl"><img src="/resource/item/google.svg" alt=""></a>
+                                <a href="https://web.telegram.org"><img src="/resource/item/telegram.svg" alt=""></a>
+                                <a href="https://en.wikipedia.org/wiki/Facebook"><img src="/resource/item/facebook.svg" alt=""></a>
+                                <a href="https://ru.wikipedia.org/wiki/Твиттер"><img src="/resource/item/twitter.svg" alt=""></a>
+                                <a href="https://vk.com/nir4y"><img src="/resource/item/Vk.svg" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                
+            </div>
+        </div>
+    </article>
 
-    </form>
-
-    <script src='main.js'></script>
+<script src="main.js"></script>
 </body>
 </html>
--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-
-let i;
-document.querySelector('.Log > button').addEventListener('click', function(){
-    const swipePanel = document.querySelector('.plate');
+let i = 1;
+const button = document.querySelectorAll('.Log > button');
+button.forEach((elem) =>{
+    elem.addEventListener('click', List)
+});
+console.log(button)
+function List(){
+    const front = document.querySelector('.front');
+    const back  = document.querySelector('.back');
     
     i++
-    swipePanel.style.right = (525) + 'px' ;
+    if(i % 2 == 0)
+    {
+        front.style.transform = 'perspective(1500px) rotateY(-180deg)' ;
+        back.style.transform = 'perspective(1500px) rotateY(-180deg)' ;
+    }
+    else
+    {
+        front.style.transform = 'perspective(1500px) rotateY(0deg)' ;
+        back.style.transform = 'perspective(1500px) rotateY(0deg)' ;
+
+    }
     
-});
+    
+};
